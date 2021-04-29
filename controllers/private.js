b7f6ed2813221dd7c16a9bd8277f110720cb77e1
SanRaph <raphytex@gmail.com>
2021-04-29 14:54:20 +0100

    create technician token successfull

--- controllers/private.js
+++ controllers/private.js
@@ -1,3 +1,7 @@
 exports.getCustomerPrivateData = ( req, res, next ) => {
     res.status(200).json({ success: true, data: 'You got access to customer private route' });
 };
+
+exports.getTechnicianPrivateData = ( req, res, next ) => {
+    res.status(200).json({ success: true, data: 'You got access to technician private route' });
+};
